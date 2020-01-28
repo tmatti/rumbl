8d98af6896bf0d6109602b5393077603488d13e4
Tim Matti <tim.matti@stord.com>
2020-01-28 17:07:14 -0500

    associate videos with a user and set up authorization

--- lib/rumbl/multimedia.ex
+++ lib/rumbl/multimedia.ex
@@ -5,19 +5,31 @@ defmodule Rumbl.Multimedia do
 
   import Ecto.Query, warn: false
   alias Rumbl.Repo
-
+  alias Rumbl.Accounts
   alias Rumbl.Multimedia.Video
 
   def list_videos do
     Repo.all(Video)
   end
 
+  def list_user_videos(%Accounts.User{} = user) do
+    Video
+    |> user_videos_query(user)
+    |> Repo.all()
+  end
+
   def get_video!(id), do: Repo.get!(Video, id)
 
+  def get_user_video!(%Accounts.User{} = user, id) do
+    Video
+    |> user_videos_query(user)
+    |> Repo.all()
+  end
 
-  def create_video(attrs \\ %{}) do
+  def create_video(%Accounts.User{} = user, attrs \\ %{}) do
     %Video{}
     |> Video.changeset(attrs)
+    |> Ecto.Changeset.put_assoc(:user, user)
     |> Repo.insert()
   end
 
@@ -33,5 +45,9 @@ defmodule Rumbl.Multimedia do
 
   def change_video(%Video{} = video) do
     Video.changeset(video, %{})
+  end
+
+  defp user_videos_query(query, %Accounts.User{id: user_id}) do
+    from(v in query, where: v.user_id == ^user_id)
   end
 end
